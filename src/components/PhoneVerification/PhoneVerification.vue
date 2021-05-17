821ac9a045760e07ffc869c3c04a1c0649ef2ab8
junminhong <junminhong1110@gmail.com>
2021-05-17 12:47:33 +0800

    新增電話驗證頁面

---
+++ src/components/PhoneVerification/PhoneVerification.vue
@@ -0,0 +1,9 @@
+<template src="./PhoneVerification.html">
+
+</template>
+
+<script>
+</script>
+
+<style src="./PhoneVerification.css" scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import CustomerForm from "@/components/CustomerForm/CustomerForm"
-
+import CustomerForm from '@/components/CustomerForm/CustomerForm'
+import PhoneVerification from '@/components/PhoneVerification/PhoneVerification'
 const routes = [
   {
     path: '/',
     name: CustomerForm,
     component: CustomerForm
+  },
+  {
+    path: '/phone-verification',
+    name: PhoneVerification,
+    component: PhoneVerification
   }
 ]
 
